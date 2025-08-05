761012685aa1f9c94b9789d7811d5b38d1fdd862
vedantmalkar <vedantitsme@gmail.com>
2025-08-05 14:35:43 +0530

    serial communication addded

---
+++ serial test/old.py
@@ -0,0 +1,29 @@
+import time
+import serial
+
+try:
+    ser = serial.Serial(
+        port= "/dev/ttyUSB0",
+        baudrate= 115200,
+        timeout=1
+    )
+
+    time.sleep(5)
+
+    try:
+        while(True):
+            ser.write(b"A")
+
+            print("The Letter 'A' has been sent!")
+            time.sleep(1)
+
+    except serial.SerialException:
+        print("port is not connected")
+
+    except KeyboardInterrupt:
+        print("STOPPED")
+
+finally:
+    if 'ser' in locals() and ser.is_open:
+        ser.close()
+        print("communication closed")
